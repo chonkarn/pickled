2dc7bd4c4ada2ce4db2a06adb30a1534ff63fe36
chonkarn <md.chkr@gmail.com>
2017-06-26 16:35:39 +0700

    CalendartoSum
    Fixed "กดบันทึกเยี่ยมบ้านจากปฏิทินได้ แต่ patient_hn เก็บค่า calendar_id
    ไปแทน"

--- calendar_summary_db.php
+++ calendar_summary_db.php
@@ -8,7 +8,7 @@ if ($sum_cancel == null) $sum_cancel = "";
 $insert = "UPDATE calendar_info SET sum_chk ='$answer'";
 $cancel = " ,comment='$sum_cancel'";
 $whereclause = " WHERE Id_app='$sum_id'";
-$summary = "INSERT INTO summary SET calendar_id='$sum_id',patient_hn='$sum_id'";
+$summary = "INSERT INTO summary SET calendar_id='$sum_id',patient_hn='$sum_hn'";
 $link = mysqli_connect("localhost", "hvmsdb", "1234", "homevisit") or die("ติดต่อไม่ได้");
  mysqli_query($link,"SET NAMES UTF8");
     mysqli_query($link,"SET character_set_results=utf8");
